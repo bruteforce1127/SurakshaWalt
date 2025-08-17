80e9fe8b444b3758b2f88756a7c4b05bfc695e5d
Adarsh dubey <adarshiiitkota@gmail.com>
2025-08-17 22:17:33 +0530

    Delete SurakshaWalt/SurakshaWalt directory

--- SurakshaWalt/SurakshaWalt/src/main/java/com/kucp1127/SurakshaWalt/UserInformation/Repository/UserInfoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.kucp1127.SurakshaWalt.UserInformation.Repository;
-
-import com.kucp1127.SurakshaWalt.UserInformation.Model.UserInfoModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserInfoRepository extends JpaRepository<UserInfoModel, String> {
-}
